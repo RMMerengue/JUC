c7892871b510556c036a4d05acc020324bd19000
RMMerengue <1017703403@qq.com>
2022-02-21 14:23:35 +0800

    线程的基础

---
+++ src/main/java/com/msb/juc/c_000/T03_Sleep_Yield_Join.java
@@ -0,0 +1,57 @@
+package main.java.com.msb.juc.c_000;
+
+public class T03_Sleep_Yield_Join {
+    public static void main(String[] args) {
+        testJoin();
+    }
+
+    static void testSleep() {
+        new Thread(()->{
+            for (int i = 0; i < 100; i++) {
+                System.out.println("A" + i);
+                try {
+                    Thread.sleep(500);
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    static void testTield() {
+        new Thread(()->{
+            for (int i = 0; i < 100; i++) {
+                System.out.println("A" + i);
+                if(i%10 == 0) Thread.yield();
+            }
+        }).start();
+
+        new Thread(()->{
+            for (int i = 0; i < 100; i++) {
+                System.out.println("----------B" + i);
+                if(i%10 == 0) Thread.yield();
+            }
+        }).start();
+    }
+
+    static void testJoin() {
+        Thread t1 = new Thread(()->{
+            for (int i = 0; i < 100; i++) {
+                System.out.println("A" + i);
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        Thread t2 = new Thread(()->{
+            try {
+                t1.join();
+            }catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+}
